48739c4bbc77bffe65c9d30f25396a5261397003
Tay Starr <tstarrroberts@gmail.com>
2022-04-28 15:30:02 -0400

    Add Heroku

---
+++ server.js
@@ -0,0 +1,46 @@
+const { animals } = require('./data/animals');
+const express = require('express');
+const app = express();
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        //  saves personalityTraits as dedicated array, if string place into new array and save
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        // loops through each trait in array
+        personalityTraitsArray.forEach(trait => {
+            // checks trait against each animal in array
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+// keep at end of code
+app.listen(3001, () => {
+    console.log(`API server now on port 3001`);
+});
