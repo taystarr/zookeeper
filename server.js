d8423658ea6b76f01412f10f59a1926904041228
Tay Starr <tstarrroberts@gmail.com>
2022-04-28 16:48:43 -0400

    ad specific animal request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const res = require('express/lib/response');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -33,12 +34,27 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        // sends 404 if requested resource not found
+        res.send(404);
+    }
 });
 
 // keep at end of code
