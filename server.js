fab54e002458dc7c87e9aecfdf089c3cb1f07fc5
Tay Starr <tstarrroberts@gmail.com>
2022-04-30 02:04:14 -0400

    connect front end

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse insoming json data
@@ -88,6 +90,22 @@ app.get('/api/animals/:id', (req, res) => {
         // sends 404 if requested resource not found
         res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
